dd75c10c85b4bea2aa86a0426d1ad3b8ebd790c5
Arjun Shenoy <arjunshenoy23@gmail.com>
2020-04-07 12:40:26 +0530

    Update movieposter.py

--- movieposter.py
+++ movieposter.py
@@ -1,3 +1,6 @@
+#this is my first attempt in doing a project.This is open to criticism
+#in this code the movie url is accepted only from IMDB.com or alternativemovieposters.com
+
 import os
 import re
 import sys
@@ -5,7 +8,6 @@ from tkinter import messagebox, Tk
 from urllib.request import urlopen
 from PIL import Image
 import webbrowser
- 
 from bs4 import BeautifulSoup
 import pyperclip
 import requests
@@ -21,22 +23,23 @@ root.withdraw()
 # Get URL of film from clipboard.
 movie_url = pyperclip.paste()
  
-# Show error and quit if not imdb address.
+# Show error and quit if the URL is not from either of these movie sites.
 if not(movie_url.startswith('https://www.imdb.com/title/') or  movie_url.startswith('https://alternativemovieposters.com/')):
     error()
  
-# Read the html source code from the URL.
+# Read the html source code from the URL copied.
 html = urlopen(movie_url)
 btfl_soup = BeautifulSoup(html.read(), 'lxml')
 print('Scraping, URL')
  
-
+#To check whether the movie is from IMDB.com or alternativemovieposters.com
 if(movie_url.startswith('https://www.imdb.com/title/')):
-        #find the poster image
-    imdb_soup = btfl_soup.find('div', {'class':'poster'})
+    
+    imdb_soup = btfl_soup.find('div', {'class':'poster'})                   #find the poster image
     cover_img = imdb_soup.find('img', {'src':re.compile('.jpg')})
 
 elif(movie_url.startswith('https://alternativemovieposters.com/')):
+ 
     rt_soup = btfl_soup.find('div', {'class':'fusion-flexslider'})   
     cover_img =rt_soup.find('img', {'src':re.compile('.jpg')})
 
@@ -51,9 +54,9 @@ if cover_img:
     cover_img_link = (cover_img['src'])
  
 # Save the jpg image from the resulting URL.
-# Note: At present this will overwtite previous scrape cover image.
+# Note: At present this will overwrite previously downloaded poster.
 if cover_img_link:
-    with open('cover.jpg', 'wb') as handle:
+    with open('poster.jpg', 'wb') as handle:
         img_response = requests.get(cover_img_link, stream=True)
         for block in img_response.iter_content(1024):
             if not block:
@@ -64,12 +67,12 @@ if cover_img_link:
 print('Image Downloaded ')
  
 
-# converting image to icon 
+# converting image to icon and saving it to the same location
 
 
-filename = r'cover.jpg'
+filename = r'poster.jpg'
 img = Image.open(filename)
-img.save('cover.ico')
+img.save('poster.ico')
 
 print('Image Converted to .ico')
 
